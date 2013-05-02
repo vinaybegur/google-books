fdc14a63a1cac31ba032e74e0ed8e94556884279
vinaybegur <vinaymbegur@gmail.com>
2013-05-02 19:43:10 +0530

    design fixes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,9 @@
 class BooksController < ApplicationController
+#  before_filter :load_api_key
+#
+#  def load_api_key
+#    @@api_key = "AIzaSyD2XAREb5Z8fP7GjLGF3yQMnOUTgx43sTg"
+#  end
 
   def index
     if params[:input_value] and !params[:input_value].blank?
@@ -8,7 +13,7 @@ class BooksController < ApplicationController
         @books_array = @key.values
         @books = Kaminari.paginate_array(@books_array.to_a).page(params[:page]).per(10)
       else
-        @books_array = GoogleBooks.search(search_condition, {:order_by => 'newest', :count => 40, :api_key => "AIzaSyD2XAREb5Z8fP7GjLGF3yQMnOUTgx43sTg"})
+        @books_array = GoogleBooks.search(search_condition, {:order_by => 'newest', :count => 40})
         @key = Key.create(:input_value => params[:input_value].to_s)
         @books_array.each_with_index do |book|
           Value.create(:key_id => @key.id, :authors => book.authors, :title => book.title, :image_link => book.image_link)
